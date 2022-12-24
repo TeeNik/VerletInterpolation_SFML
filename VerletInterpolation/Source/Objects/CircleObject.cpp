e99084d3c63fc6884128037c7ad7479489075ea7
TeeNik <teenik3315@gmail.com>
2022-12-24 16:57:34 +0600

    add gravity

--- VerletInterpolation/Source/Objects/CircleObject.cpp
+++ VerletInterpolation/Source/Objects/CircleObject.cpp
@@ -2,16 +2,34 @@
 
 CircleObject::CircleObject()
 {
-	Shape.setRadius(80.f);
-	Shape.setPosition(100.f, 100.f);
-	Shape.setFillColor(sf::Color::Blue);
+	shape.setRadius(80.f);
+	shape.setPosition(100.f, 100.f);
+	shape.setFillColor(sf::Color::Blue);
+	shape.setOutlineThickness(10.f);
+	shape.setOutlineColor(sf::Color(250, 150, 100));
+
+	position_currect = {};
+	position_old = {};
 }
 
-void CircleObject::Update(float DeltaTime)
+void CircleObject::Update(float deltaTime)
 {
+	const Vector2 gravity(0, 1000);
+	Accelerate(gravity);
+	
+	const Vector2 velocity = position_currect - position_old;
+	position_old = position_currect;
+	position_currect = position_currect + velocity + acceleration * deltaTime * deltaTime;
+	acceleration = Vector2{0,0};
 }
 
 void CircleObject::Render(sf::RenderWindow& window)
 {
-	window.draw(Shape);
+	shape.setPosition(position_currect.x, position_currect.y);
+	window.draw(shape);
+}
+
+void CircleObject::Accelerate(Vector2 acc)
+{
+	acceleration = acceleration + acc;
 }
